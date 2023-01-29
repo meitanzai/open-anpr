8f6cafb43b0cf96e07ee87d95e4ac6c2ce302ac8
diven <divenwu@kuainiugroup.com>
2023-01-29 14:03:43 +0800

    init

---
+++ scripts/docker_build.sh
@@ -0,0 +1,15 @@
+version='1.0.0'
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+cd ${SHELL_FOLDER}
+
+#编译项目
+cd ../
+mvn clean package -DskipTests -Pdocker
+
+#编译
+docker build -f scripts/docker/Dockerfile -t visual/open-anpr:${version} .
+docker tag visual/open-anpr:${version} visual/open-anpr:latest
+
+#中央仓库
+docker tag visual/open-anpr:${version} divenswu/open-anpr:${version}
+docker tag visual/open-anpr:${version} divenswu/open-anpr:latest
